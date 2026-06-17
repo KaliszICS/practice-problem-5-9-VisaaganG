c6a59766a46ea7c6a930c115b28dfc829481cbd5
VisaaganG <343505582@tdsb.ca>
2026-06-17 03:47:16 +0000

    Questions Partly Finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,26 @@
+import java.util.ArrayDeque;
+
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
-	}
-
+	public static String processBackspaces(String input) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+ 
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '#') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+	String result = "";
+        while (!stack.isEmpty()) {
+            result = stack.pop() + result;
+        }
+        return result;
+    }
+ 
 }
